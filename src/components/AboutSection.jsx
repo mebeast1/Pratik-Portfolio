c5ab7f7da714f3a714fca1e11744cc0e3504d40a
mebeast1 <mrsam992000@gmail.com>
2025-06-05 12:09:02 +0530

    added resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="/public/pratik_resume.pdf" download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
